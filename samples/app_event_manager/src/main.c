9876da09828b5c552f7ff9bef1e5eadc94f09326
Kari Hamalainen <91663826+karhama@users.noreply.github.com>
2023-08-04 14:10:11 +0300

    Update west.yml

--- samples/app_event_manager/src/main.c
+++ samples/app_event_manager/src/main.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2019 Nordic Semiconductor ASA
  *
- * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause 
  */
 
 #include <app_event_manager.h>
