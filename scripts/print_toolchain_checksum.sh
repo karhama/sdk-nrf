ab47958f955944145f18bd5b48e765df20e35f13
Kari Hamalainen <kari.hamalainen@nordicsemi.no>
2025-08-11 09:10:14 +0300

    ci: add multiplatform twister
    Signed-off-by: Kari Hamalainen <kari.hamalainen@nordicsemi.no>

--- scripts/print_toolchain_checksum.sh
+++ scripts/print_toolchain_checksum.sh
@@ -2,7 +2,14 @@
 
 BASEDIR=$(dirname "$0")
 REQUIREMENTS=$BASEDIR/requirements-fixed.txt
-TOOLS_VERSIONS=$BASEDIR/tools-versions-linux.yml
-TOOLCHAIN_VERSION=$(cat $REQUIREMENTS $TOOLS_VERSIONS | tr -d '\r' | sha256sum | head -c 10)
+
+# Detect OS and set tools version file
+case "$(uname -s)" in
+    Linux*)   TOOLS_VERSIONS="$BASEDIR/tools-versions-linux.yml" ;;
+    Darwin*)  TOOLS_VERSIONS="$BASEDIR/tools-versions-darwin.yml" ;;
+    *) echo "Unsupported OS"; exit 1 ;;
+esac
+# Compute SHA256 and take first 10 characters
+TOOLCHAIN_VERSION=$(cat "$REQUIREMENTS" "$TOOLS_VERSIONS" | sed 's/\r$//' | shasum -a 256 | cut -c1-10)
 
 echo "${TOOLCHAIN_VERSION}"
